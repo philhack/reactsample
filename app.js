ce32ffdabc86231697b6f17fe2134ce8116edb52
Phil Hack <phil@philhack.com>
2015-05-24 21:24:21 +0100

    make the game working an dupdated it to handle events

--- app.js
+++ app.js
@@ -4,38 +4,61 @@ var Quiz = React.createClass({
     propTypes: {
       data: React.PropTypes.array.isRequired
     },
-    getInitialState: function (){
-        return this.props.data.selectGame();
+    getInitialState: function () {
+        return _.extend({
+            bgClass: 'neutral',
+            showContinue: false,
+        }, this.props.data.selectGame());
     },
-    render: function() {
-        return <div>
-            <div className="row">
-                <div className="col-md-4">
-                    <img src={this.state.author.imageUrl} className="authorImage" col-md-3 />
+    handleBookSelected: function(title) {
+        var isCorrect = this.state.checkAnswer(title);
+        this.setState({
+            bgClass: isCorrect ? 'pass' : 'fail',
+            showContinue: isCorrect
+        });
+    },
+    handleContinue: function () {
+        this.setState(this.getInitialState());
+    },
+
+    render: function () {
+        return (<div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src={this.state.author.imageUrl} className="authorimage col-md-3" />
+                    </div>
+                    <div className={"col-md-7"}>
+                        {this.state.books.map(function (b) {
+                            return <Book onBookSelected={this.handleBookSelected} key={b} title={b} />;
+                        },this)}
+                    </div>
+                    <div className={"col-md-1 " + this.state.bgClass} />
                 </div>
-                <div className="col-md-7">
-                    {this.state.books.map(function(b){
-                        return <Book title={b} />;
-                    }, this)}
-                </div>
-                <div className="col-md-1">
-                </div>
+                {this.state.showContinue ? (
+                    <div className="row">
+                        <div className="col-md-12">
+                            <input onClick={this.handleContinue} type="button" className="btn btn-primary btn-lg pull-right" value="Continue" />
+                        </div>
+                    </div>) : <span/>
+                }
             </div>
-        </div>;
-}});
+        );
+    }
+});
 
 
 var Book = React.createClass({
-    propTypes:{
+    propTypes: {
         title: React.PropTypes.string.isRequired
     },
-   render: function(){
-        return <div className="answer">
-                    <h4>
-                        {this.props.title}
-                    </h4>
-                </div>;
-   }
+    handleClick: function () {
+        this.props.onBookSelected(this.props.title);
+    },
+    render: function () {
+        return  <div onClick={this.handleClick} className="answer">
+            <h4>{this.props.title}</h4>
+        </div>;
+    }
 });
 
 var data = [
@@ -77,10 +100,10 @@ var data = [
     }
 ];
 
-data.selectGame = function(){
-    var books = _.shuffle(this.reduce(function(p, c, i){
+data.selectGame = function () {
+    var books = _.shuffle(this.reduce(function (p, c, i) {
         return p.concat(c.books);
-    }, [])).slice(0, 4);
+    }, [])).slice(0,4);
 
     var answer = books[_.random(books.length-1)];
 
@@ -90,9 +113,16 @@ data.selectGame = function(){
             return author.books.some(function (title) {
                 return title === answer;
             });
-        })
-    }
+        }),
+
+        checkAnswer: function (title) {
+            return this.author.books.some(function (t) {
+                return t === title;
+            });
+        }
+    };
 };
+
 
 React.render(<Quiz data={data} />,
     document.getElementById('app'));
