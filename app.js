c898da576c58883a77b35b3739d4db25cb543594
Phil Hack <phil@philhack.com>
2015-05-24 18:59:42 +0100

    added a list of books as an array and displaying the list of books

--- app.js
+++ app.js
@@ -3,9 +3,20 @@
 var Quiz = React.createClass({
     render: function() {
         console.log('rendering');
-        return <div>{this.props.name}</div>;
-    }
+        return <div>
+            {this.props.books.map(function(b){
+                return <Book title={b} />;
+            })}
+        </div>;
+}});
+
+
+var Book = React.createClass({
+   render: function(){
+        return <div><h4>{this.props.title}</h4></div>;
+   }
 });
 
-React.render(<Quiz name="foo" />, document.getElementById('app'));
+React.render(<Quiz books={['The Lord of the Rings', 'The Iliad']} />,
+    document.getElementById('app'));
 
